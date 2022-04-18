dc2a157915dbac89a1d15d2a899dc9dc16b678d6
Bharat Dua <bharatdua13@gmail.com>
2022-04-18 12:38:51 +0530

    completed add function

--- js/Notesapp.js
+++ js/Notesapp.js
@@ -1,1 +1,44 @@
-console.log('this is fired');
+console.log("this is fire");
+
+let addBtn = document.getElementById("addBtn");
+
+addBtn.addEventListener("click", (e) => {
+  let addTxt = document.getElementById("addTxt");
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  notesObj.push(addTxt.value);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  addTxt.value = "";
+  //   console.log(notes);
+  showNotes();
+});
+function showNotes() {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  let html = "";
+  notesObj.forEach((element, index) => {
+    html += `
+        <div id="notes" class="row container-fluid">
+        <div class="card my-3 mx-3 noteCard" style="width: 18rem">
+          <div class="card-body">
+            <h5 class="card-title"> Note ${index + 1}</h5>
+            <p class="card-text">${element}</p>
+            <button class="btn btn-primary">Delete Note </button>
+          </div>
+        </div>
+    </div>
+        `;
+  });
+  let notesElem = document.getElementById("notes");
+  if (notesObj.length != 0) {
+    notesElem.innerHTML = html;
+  }
+}
