e78262538dac295bff3f43544d49cc7a955e6121
Bharat Dua <bharatdua13@gmail.com>
2022-04-19 13:34:09 +0530

    add title functionality

--- js/Notesapp.js
+++ js/Notesapp.js
@@ -5,6 +5,7 @@ showNotes();
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", (e) => {
   let addTxt = document.getElementById("addTxt");
+  let addTitle = document.getElementById("addTitle");
   let notes = localStorage.getItem("notes");
   if (notes == null) {
     // it's checking the condition that if there is any  notes in local storage
@@ -12,9 +13,14 @@ addBtn.addEventListener("click", (e) => {
   } else {
     notesObj = JSON.parse(notes);
   }
-  notesObj.push(addTxt.value);         
+  let myObj = {
+    title: addTitle.value,
+    text: addTxt.value,
+  };
+  notesObj.push(myObj);
   localStorage.setItem("notes", JSON.stringify(notesObj));
   addTxt.value = "";
+  addTitle.value = "";
   showNotes();
 });
 function showNotes() {
@@ -30,10 +36,9 @@ function showNotes() {
         <div id="notes" class="row container-fluid">
         <div class="card my-3 mx-3 noteCard" style="width: 18rem">
           <div class="card-body">
-            <h5 class="card-title"> Note ${index + 1}</h5>
-            <p class="card-text">${element}</p>
+            <h5 class="card-title">  ${element.title}</h5>
+            <p class="card-text">${element.text}</p>
             <button onclick = "deleteNotes(this.id)"  id = "${index}" class="btn btn-primary" >Delete Note </button>
-             
           </div>
         </div>
     </div>
@@ -62,7 +67,6 @@ function deleteNotes(index) {
 let search = document.getElementById("searchTxt");
 search.addEventListener("input", () => {
   let inputVal = search.value.toLowerCase();
-
   let notesCard = document.getElementsByClassName("noteCard");
   Array.from(notesCard).forEach((element) => {
     let cardTxt = element.getElementsByTagName("p")[0].innerText;
